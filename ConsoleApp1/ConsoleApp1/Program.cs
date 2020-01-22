0fe0d09dffabe03c743b75b70060405aa3483c01
Domingo Javier Martinez Torrivilla <Domingo_Martinez@Dell.com>
2020-01-22 17:22:54 -0600

    firts example

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Press any key when ready.");
+            Console.ReadLine();
+            await foreach(var e in MessagesAsync()) {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static IEnumerable<string> Messages() {
+            yield return "Hello!";
+            yield return "world!";
+        }
+
+        public async IAsyncEnumerable<string> MessagesAsync() {
+            await Task.Delay(2000);
+            yield return "Hello!";
+            await Task.Delay(2000);
+            yield return "World!";
+        }
+    }
+}
